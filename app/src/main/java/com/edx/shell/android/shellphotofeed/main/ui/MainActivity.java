f21bd1687bbc9294c9a2ca4fb675cd7ed2af38a7
Cesar Morales Garduño <cesar.uam.computacion@gmail.com>
2016-06-28 17:58:20 -0500

    Implementación de la actividad principal.
    - Se inicializó la implementación de las funciones principales de la vista de fotografías.
    - Se creo el adaptador de la lista de fragmentos que contendrá la vista principal.
    -Se creó el menu con la opción de deslogueo de la sesión.

--- app/src/main/java/com/edx/shell/android/shellphotofeed/main/ui/MainActivity.java
+++ app/src/main/java/com/edx/shell/android/shellphotofeed/main/ui/MainActivity.java
@@ -1,15 +1,167 @@
 package com.edx.shell.android.shellphotofeed.main.ui;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.location.Location;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
+import com.edx.shell.android.shellphotofeed.PhotoFeedApp;
 import com.edx.shell.android.shellphotofeed.R;
+import com.edx.shell.android.shellphotofeed.login.ui.LoginActivity;
+import com.edx.shell.android.shellphotofeed.main.MainPresenter;
+import com.edx.shell.android.shellphotofeed.main.ui.adapters.MainSectionsPagerAdapter;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
 
-public class MainActivity extends AppCompatActivity {
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity implements MainView,
+                                                                GoogleApiClient.ConnectionCallbacks,
+                                                                GoogleApiClient.OnConnectionFailedListener {
+
+    // Variables
+    private PhotoFeedApp app;
+    private Location lastKnownLocation;
+    private GoogleApiClient apiClient;
+
+    // Servicios
+    MainPresenter presenter;
+    MainSectionsPagerAdapter adapter;
+    SharedPreferences sharedPreferences;
+
+    // Componentes
+    @Bind(R.id.toolbar)
+    Toolbar toolbar;
+    @Bind(R.id.tabs)
+    TabLayout tabs;
+    @Bind(R.id.viewPager)
+    ViewPager viewPager;
+    @Bind(R.id.fab)
+    FloatingActionButton fab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        app = (PhotoFeedApp) getApplication();
+        setupInjection();
+        setupNavigation();
+        setupGoogleAPIClient();
+
+        presenter.onCreate();
+    }
+
+    private void setupInjection() {
+        // TODO
+    }
+
+    private void setupNavigation() {
+        String email  =sharedPreferences.getString(app.getEmailKey(), getString(R.string.app_name));
+        toolbar.setTitle(email);
+        setSupportActionBar(toolbar);
+
+        viewPager.setAdapter(adapter);
+        tabs.setupWithViewPager(viewPager);
+    }
+
+    private void setupGoogleAPIClient() {
+        if (apiClient == null) {
+            apiClient = new GoogleApiClient.Builder(this)
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .addApi(LocationServices.API)
+                    .build();
+
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        apiClient.connect();
+    }
+
+    @Override
+    protected void onStop() {
+        apiClient.disconnect();
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        presenter.onDestroy();
+        super.onDestroy();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_logout:
+                logout();
+                break;
+            default:
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void logout() {
+        presenter.logout();
+        sharedPreferences.edit()
+                .clear()
+                .commit();
+        Intent intent = new Intent(this, LoginActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP
+                | Intent.FLAG_ACTIVITY_NEW_TASK
+                | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+    }
+
+    @Override
+    public void onUploadInit() {
+
+    }
+
+    @Override
+    public void onUploadComplete() {
+
+    }
+
+    @Override
+    public void onUploadError(String error) {
+
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
     }
 }
